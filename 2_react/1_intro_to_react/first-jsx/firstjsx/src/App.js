6266b6f75854339f15cb3c6ee8a48e0a6847a15b
chuang-ds <chet.huang.vt@gmail.com>
2021-08-08 15:04:32 -0700

    first jsx

---
+++ 2_react/1_intro_to_react/first-jsx/firstjsx/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <>
+            <h1 class="My-header">Hello Dojo</h1>
+            <p>Chet's bucket list:</p>
+            <ul>
+              <li>Learn React</li>
+              <li>Mountain biking trip</li>
+              <li>Wild hogs ride across route 66</li>
+              <li>Paddle down the Yukon</li>
+              <li>Hot air balloon in the Autumn</li>
+              </ul>        
+        </>
+    </div>
+  );
+}
+
+export default App;
